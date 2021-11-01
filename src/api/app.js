f99279bbcf0c798548e35b861d25068df861526a
Daniel Leon <daniel.radialista@gmail.com>
2021-11-01 14:44:11 -0300

    colocando as pastas dentro da pasta src

--- src/api/app.js
+++ src/api/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { users, login, recipes } = require('../../routes');
+const { users, login, recipes } = require('../routes');
 
 const app = express();
 app.use(bodyParser.json());
